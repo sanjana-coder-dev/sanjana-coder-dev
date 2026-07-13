cf4797bc9fb1f682ee381982697d2871a5c6c57f
sanjana T <sanjut0138@gmail.com>
2026-07-13 20:28:34 +0530

    Add files via upload
    Variables
    Data Type
    User input
    Output using print()

---
+++ student-information-system.py
@@ -0,0 +1,9 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+cgpa=float(input("Enter your CGPA: "))
+hosteller=input("Are you a hosteller? (True/False): ")=="True"
+print("\n-- Student Details ---")
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("Hosteller:", hosteller)
